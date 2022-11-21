f9116b685c6779362638148095d1f3999e61d084
yiouliu1998 <yiouliu2333@gmail.com>
2022-11-21 16:33:01 +0800

    first commit

---
+++ battery.py
@@ -0,0 +1,19 @@
+# This is a python program for battery analysis in nda file
+# In this program, you can get some useful information
+# It is created by ZhiHang
+
+class Sy_battery:
+class Full_battery:
+class Battery:
+    def __init__ (self, cycles, Healthe):
+
+def main():
+    info = get_batteryinfo()
+    plot = battery_plot()
+
+def get_batteryinfo():
+
+def battery_plot():
+
+if __name__ = "__main__":
+    main()
